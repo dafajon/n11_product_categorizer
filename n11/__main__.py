f4fc3caf17fd519bd1549f3e98ccd768d8e5006e
dafajon <dorukhan.afacan@globalmaksimum.com>
2021-11-02 17:53:37 +0300

    Implement training pipeline.

--- n11/__main__.py
+++ n11/__main__.py
@@ -1,12 +1,57 @@
 import click
 import uvicorn
+import datetime
+import joblib
+import pandas as pd
+from rich.console import Console
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
 
 from .inference_server import make_restful_api
+from .utils import make_tokenizer
+
+today = datetime.today().strftime('%Y-%m-%d')
+console = Console()
 
 @click.group(help="N11 Product Categorizer Commandline")
 def cli():
     pass
 
+@click.option("-d", "--datapath", help="Path to dataset")
+@click.option("-x", "--text-col", help="Text column", default="DESCRIPTION")
+@click.option("-t", "--title-col", help="Title column", default="TITLE")
+@click.option("-l", "--label-col", help="Label column", default="CATEGORY_ID")
+def train(datapath: str, text_col: str, title_col: str, label_col: str):
+    try:
+        import nltk
+        nltk.download('stopwords')
+        from nltk.corpus import stopwords
+        stopwords = set(stopwords.words('turkish'))
+    except:
+        raise ImportError("NLTK not available")
+
+    console.log("Reading data...")
+
+    df = pd.read_csv(datapath, nrows=5_000)
+
+    column_trans = ColumnTransformer(
+        [("word_ngram", TfidfVectorizer(strip_accents='unicode', ngram_range=(1, 2), stop_words=stopwords, sublinear_tf=True, smooth_idf=False, tokenizer=make_tokenizer(5)), [text_col, title_col]),
+         ("char_ngram", TfidfVectorizer(strip_accents='unicode', ngram_range=(1, 3), stop_words=stopwords, sublinear_tf=True, smooth_idf=False, analyzer="char"), [text_col, title_col])]
+
+    )
+
+    pipe = Pipeline([("column_transformer", column_trans),
+                     ("linear_svc", LinearSVC(C=3.995737425961318, intercept_scaling=2.7229092594601005))])
+
+    console.log("Fitting pipeline...")
+    pipe.fit(df[[text_col, title_col]], df[label_col])
+
+    joblib.dump(pipe, f"n11/models/pipeline_{today}.joblib")
+
+    console.log("Pipeline saved!")
 
 @cli.command(help="Serve a trained and dumped model")
 @click.option("-m", "--model", help="Name of the model to be served")
--- n11/utils/_process.py
+++ n11/utils/_process.py
@@ -1,4 +1,9 @@
 import re
+from sadedegel.bblock.word_tokenizer import ICUTokenizer
+from functools import partial
+
+tokenizer = ICUTokenizer()
+
 
 def process_st_input(user_input: str):
     user_input = re.sub("\n+", "\n", user_input)
@@ -7,3 +12,13 @@ def process_st_input(user_input: str):
     request_body = [{"texts": texts}]
 
     return request_body
+
+
+def custom_tokenizer(doc: str, prefix_len: int, tokenizer=tokenizer):
+        #tokens = [t.lower_ for t in tokenizer(doc)]
+        tokens = [t.lower_[:prefix_len] for t in tokenizer(doc)]
+        return tokens
+
+def make_tokenizer(prefix_len):
+    ct = partial(custom_tokenizer, prefix_len=prefix_len)
+    return ct
